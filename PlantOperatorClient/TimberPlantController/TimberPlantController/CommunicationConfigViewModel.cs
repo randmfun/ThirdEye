e982a7718859619110e1d301b95058a7c5c1e388
Jacob Aloysious <jacobaloysious@gmail.com>
2014-03-13 14:00:08 +0530

    First version of client

---
+++ PlantOperatorClient/TimberPlantController/TimberPlantController/CommunicationConfigViewModel.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+
+namespace TimberPlantController
+{
+    public class CommunicationConfigViewModel: INotifyPropertyChanged
+    {
+        public List<string> ComPorts { get; set; }
+        public List<string> BaudRates { get; set; }
+
+        public string SelectedComPort { get; set; }
+        public string SelectedBaudRate { get; set; }
+
+        public CommunicationConfigViewModel()
+        {
+            ComPorts= GetComPorts();
+            BaudRates = GetBaudRate();
+
+            SelectedComPort = this.ComPorts.First();
+            SelectedBaudRate = this.BaudRates.First();
+
+            this.radioStopBits = getRadioStopBits();
+            this.radioParity = GetRadioPartiyBits();
+            this.radioDataBits = GetRadioDataBits();
+        }
+
+        public string SelectedDataBit
+        {
+            get { return this.RadioDataBits.Find(item => item.CheckedProperty).Header; }
+        }
+
+        public string SelectedParity
+        {
+            get { return this.RadioParity.Find(item => item.CheckedProperty).Header; }
+        }
+
+        public string SelectedStopBit
+        {
+            get { return this.RadioStopBits.Find(item => item.CheckedProperty).Header; }
+        }
+
+        private List<RadioClass> radioDataBits;
+        public List<RadioClass> RadioDataBits
+        {
+            get { return radioDataBits; }
+            set { radioDataBits = value;
+            this.OnPropertyChanged("RadioDataBits");
+            }
+        }
+
+        private List<RadioClass> radioParity;
+        public List<RadioClass> RadioParity
+        {
+            get { return radioParity; }
+            set { radioParity = value;
+                this.OnPropertyChanged("RadioParity");
+            }
+        }
+
+        private List<RadioClass> radioStopBits;
+        public List<RadioClass> RadioStopBits
+        {
+            get
+            {
+                return radioStopBits;
+            }
+            set
+            {
+                radioStopBits = value;
+                this.OnPropertyChanged("RadioStopBits");
+            }
+        }
+
+        private List<RadioClass> getRadioStopBits()
+        {
+            List<RadioClass> list = new List<RadioClass>();
+            list.Add(new RadioClass { Header = "1", CheckedProperty = false });
+            list.Add(new RadioClass { Header = "1.5", CheckedProperty = false });
+            list.Add(new RadioClass { Header = "2", CheckedProperty = false });
+            return list;
+        }
+
+        private List<RadioClass> GetRadioPartiyBits()
+        {
+            List<RadioClass> list = new List<RadioClass>();
+            list.Add(new RadioClass { Header = "No", CheckedProperty = false });
+            list.Add(new RadioClass { Header = "Odd", CheckedProperty = false });
+            list.Add(new RadioClass { Header = "Even", CheckedProperty = false });
+            return list;
+        }
+
+        private List<RadioClass> GetRadioDataBits()
+        {
+            List<RadioClass> list = new List<RadioClass>();
+            list.Add(new RadioClass { Header = "5", CheckedProperty = false });
+            list.Add(new RadioClass { Header = "6", CheckedProperty = false });
+            list.Add(new RadioClass { Header = "7", CheckedProperty = false });
+            list.Add(new RadioClass { Header = "8", CheckedProperty = false });
+            return list;            
+        }
+
+        private static List<string> GetComPorts()
+        {
+            return SerialPort.GetPortNames().ToList();
+        }
+
+        private static List<string> GetBaudRate()
+        {
+            return new List<string> {"1200","2400","4800","9600", "19200","38400","57600","115200","128000","256000"};
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RadioClass : INotifyPropertyChanged
+    {
+        private string header;
+        public string Header
+        {
+            get { return header; }
+            set
+            {
+                header = value;
+                this.OnPropertyChanged("Header");
+            }
+        }
+
+        private bool checkedProperty;
+        public bool CheckedProperty
+        {
+            get { return checkedProperty; }
+            set
+            {
+                checkedProperty = value;
+                this.OnPropertyChanged("CheckedProperty");
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ PlantOperatorClient/TimberPlantController/TimberPlantController/CommunicationSetup.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TimberPlantController
+{
+    /// <summary>
+    /// Interaction logic for CommunicationSetup.xaml
+    /// </summary>
+    public partial class CommunicationSetup : Window
+    {
+        CommunicationConfigViewModel communicationConfigViewModel = new CommunicationConfigViewModel();
+        public CommunicationSetup()
+        {
+            InitializeComponent();
+            this.DataContext = communicationConfigViewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var s = this.communicationConfigViewModel.SelectedDataBit;
+            var d = this.communicationConfigViewModel.SelectedStopBit;
+        }
+
+    }
+}
