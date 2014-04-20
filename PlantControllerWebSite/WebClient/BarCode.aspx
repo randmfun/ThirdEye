20d0efb7b3d9d0162db1a4fc441e96955e24ddc5
Jacob Aloysious <jacobaloysious@gmail.com>
2014-04-20 14:09:27 +0530

    Initial version of Web Client

---
+++ PlantControllerWebSite/WebClient/BarCode.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Generate Bar Code" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="BarCode.aspx.cs" Inherits="WebClient.BarCode" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Bar Code
+    </h2>
+    <p>
+        <asp:Label ID="lblBinFile" runat="server" Text="Bin File"></asp:Label>
+        <asp:TextBox ID="txtBarCodeFile" runat="server"></asp:TextBox>
+        
+        <br />
+        <asp:Button ID="btnGenBarCode" runat="server" Text="Generate" />
+        <br />
+
+        <asp:Image ID="imgBarCode" runat="server" />
+        
+    </p>
+</asp:Content>
---
+++ PlantControllerWebSite/WebClient/BarCode.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Randmfun.BarCodeGenerator;
+
+namespace WebClient
+{
+    public partial class BarCode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            imgBarCode.ImageUrl = GetImage("");
+        }
+
+        public string GetImage(object img)
+        {
+            Code128Content content = new Code128Content("hello");
+            var text = string.Join(string.Empty, content.Codes);
+
+            System.Drawing.Image myimg = Code128Rendering.MakeBarcodeImage("hello", 2, true);
+
+            MemoryStream ms = new MemoryStream();  // no using here! BitmapImage will dispose the stream after loading
+            myimg.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            
+            return "data:image/bmp;base64," + Convert.ToBase64String((byte[])ms.GetBuffer());
+        }
+    }
+}
